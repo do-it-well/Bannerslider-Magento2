1401ffa5bc6f963a819acf1ead698e8748eebbe5
Will Palmer <will@do-it-well.co.uk>
2018-04-24 22:14:51 +0100

    Specify the type for the $connection argument
    In Magento 2.2.x, the $connection argument is not automatically detected
    as a database adapter when building objects with a factory. This causes,
    among other things, the following error during di compilation:
    
    Incompatible argument type:
        Required type: \Magento\Framework\DB\Adapter\AdapterInterface.
        Actual type: \Magento\Framework\Model\ResourceModel\Db\AbstractDb;
        File: ... Model/ResourceModel/Banner/Collection.php
    
    Specifying the expected type of the $connection argument allows di
    compilation to succeed.

--- Model/ResourceModel/Banner/Collection.php
+++ Model/ResourceModel/Banner/Collection.php
@@ -77,7 +77,7 @@ class Collection extends \Magento\Framework\Model\ResourceModel\Db\Collection\Ab
         \Magento\Framework\Data\Collection\Db\FetchStrategyInterface $fetchStrategy,
         \Magento\Framework\Event\ManagerInterface $eventManager,
         \Magento\Store\Model\StoreManagerInterface $storeManager,
-        $connection = null,
+        \Magento\Framework\DB\Adapter\AdapterInterface $connection = null,
         \Magento\Framework\Model\ResourceModel\Db\AbstractDb $resource = null
     ) {
         parent::__construct($entityFactory, $logger, $fetchStrategy, $eventManager, $connection, $resource);
